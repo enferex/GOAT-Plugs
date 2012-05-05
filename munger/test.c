dd4b93c22720ad2e79937f0fd737c3791c375bdd
Matt Davis <mattdavis9@gmail.com>
2012-05-05 10:02:37 +1000

    Updated test to stress reuse of encoded data.

--- munger/test.c
+++ munger/test.c
@@ -8,8 +8,17 @@ const char * const foo3 = "Bazzzzzzzz!3";
 char *foo100 = "FOooO100";
 char fooX = 'X';
 
+static void test2(void)
+{
+    printf("Testing1!\n");
+    printf("Testing2!\n");
+}
+
 int main(void)
 {
-   printf("Testing!\n");
-   return 0; 
+    printf("Testing1!\n");
+    printf("Testing2!\n");
+    test2();
+
+    return 0; 
 }
